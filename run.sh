9a5ab88c24ce1357e17981c149e040c9f239c94e
johnny850807 <johnny850807@gmail.com>
2021-03-07 22:22:57 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+docker-compose -f infra.yml up -d
 ./mvnw clean package -DskipTests
 cd Judger/ && docker build . -t judger -f Dockerfile.cqi && cd ..
-docker-compose -f all-in-one.yml build
-docker-compose -f all-in-one.yml up
+docker-compose -f services.yml build
+docker-compose -f services.yml up
