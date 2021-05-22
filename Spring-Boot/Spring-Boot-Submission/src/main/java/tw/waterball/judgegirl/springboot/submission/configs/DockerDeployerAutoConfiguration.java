7591b1eea1bbade7ddf8e4b8e2dae6d5e8dc226b
wally <wally55077@gmail.com>
2021-05-22 17:45:19 +0800

    Use Spring @Schedule instead of ScheduledExecutorService (#333)

--- Spring-Boot/Spring-Boot-Submission/src/main/java/tw/waterball/judgegirl/springboot/submission/configs/DockerDeployerAutoConfiguration.java
+++ Spring-Boot/Spring-Boot-Submission/src/main/java/tw/waterball/judgegirl/springboot/submission/configs/DockerDeployerAutoConfiguration.java
@@ -21,18 +21,10 @@ import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tw.waterball.judgegirl.springboot.configs.properties.JudgeGirlAmqpProps;
-import tw.waterball.judgegirl.springboot.configs.properties.JudgeGirlJudgerProps;
-import tw.waterball.judgegirl.springboot.configs.properties.ServiceProps;
-import tw.waterball.judgegirl.springboot.submission.impl.deployer.docker.DockerJudgerDeployer;
-import tw.waterball.judgegirl.submission.deployer.JudgerDeployer;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author - johnny850807@gmail.com (Waterball)
@@ -40,29 +32,10 @@ import java.util.concurrent.ScheduledExecutorService;
 @ConditionalOnProperty(name = "judge-girl.judger.strategy",
         havingValue = DockerDeployerAutoConfiguration.STRATEGY)
 @Configuration
+@EnableScheduling
 public class DockerDeployerAutoConfiguration {
     public static final String STRATEGY = "docker";
     private static final Logger logger = LogManager.getLogger();
-
-    @Bean
-    public ScheduledExecutorService scheduler() {
-        logger.info("Allocating ScheduledExecutorService with thread pool size=2.");
-        return Executors.newScheduledThreadPool(2);
-    }
-
-    @Bean
-    public JudgerDeployer kubernetesJudgerDeployer(@Value("${judge-girl.judger.docker.dockerRemovalIntervalInMs}")
-                                                           int dockerRemovalIntervalInMs,
-                                                   @Value("${jwt.secret}") String jwtSecret,
-                                                   DockerClient dockerClient,
-                                                   ScheduledExecutorService scheduler,
-                                                   ServiceProps.ProblemService problemServiceProps,
-                                                   ServiceProps.SubmissionService submissionServiceProps,
-                                                   JudgeGirlAmqpProps amqpProp,
-                                                   JudgeGirlJudgerProps deployProps) {
-        return new DockerJudgerDeployer(dockerRemovalIntervalInMs, jwtSecret, dockerClient, scheduler,
-                problemServiceProps, submissionServiceProps, amqpProp, deployProps);
-    }
 
     @Bean
     public DockerClientConfig dockerClientConfig() {
